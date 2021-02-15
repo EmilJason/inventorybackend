1969ab0f7de5d7ce2bae498da34238d59bf4429d
Emil Jason DC. Datuin <emiljasondatuin@gmail.com>
2021-02-15 08:40:16 +0800

    stock functionality added

--- router/stock.router.js
+++ router/stock.router.js
@@ -16,13 +16,15 @@ router.get("/api/stock", (req, res) => {
   }
 });
 
-router.post("/api/stock/add", (req, res) => {
-  let newStock = new stockModel({
-    productID: req.body.id,
-    description: req.body.description,
-    quantity: req.body.quantity,
+router.put("/api/stock/new/:id", (req, res) => {
+  let { quantity } = req.body;
+  stockModel.findOne({ productID: req.params.id }).then((result) => {
+    let qty = Number(result.quantity) + Number(quantity);
+    stockModel
+      .findByIdAndUpdate(result._id, { quantity: qty })
+      .then(() => res.send("saved successfully"))
+      .catch((e) => res.send(e.message));
   });
-  newStock.save().then(() => res.send(newStock._id));
 });
 
 module.exports = router;
